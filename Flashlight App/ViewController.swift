9c985dbef4f80bbfed2571897454688ce75cad10
Emmett <ekennedy@cps.edu>
2021-07-19 12:31:57 -0500

    Flashlight App

--- Flashlight App/ViewController.swift
+++ Flashlight App/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        
+        if(lightOn){
+            view.backgroundColor = .white
+        }
+        else{
+            view.backgroundColor = .black
+        }
+    }
+    
 }
 
